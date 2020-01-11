2eb7449c5f0530da66c6bfc2daecad38e767e176
Vishnu Menon <54286371+vishnumnn@users.noreply.github.com>
2020-01-11 16:40:05 -0600

    Schemas (restaurant and shelter)

---
+++ api/schemas/restaurant.js
@@ -0,0 +1,11 @@
+module.exports = {
+    id : Number,
+    location : String,
+    name : String,
+    foodExcess : Array,
+    mostRecentExcessEntry : Date,
+    mostRecentFoodSubmission : Date,
+    transactionHistory : Array,
+    phoneNumber : String,
+    email : String
+}
---
+++ api/schemas/shelter.js
@@ -0,0 +1,10 @@
+module.exports = {
+    id : Number,
+    location : String, 
+    name : String,
+    phoneNumber : String,
+    email : String,
+    foodServed : Array,
+    demographics : Object,
+    transactionHistory : Array
+}
