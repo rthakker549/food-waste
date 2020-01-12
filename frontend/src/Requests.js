b1a50eaff01f9d47fac4c7ff8b914c544898713d
rthakker549 <rthakker549@gmail.com>
2020-01-12 08:18:17 -0800

    Fixed Requests

--- frontend/src/Requests.js
+++ frontend/src/Requests.js
@@ -6,24 +6,19 @@ import { Form, FormInput, FormGroup, FormSelect, Button } from "shards-react";
 import axios from "axios";
 
 class Requests extends React.Component {
-    state = {
-        date: new Date(),
-    }
+
 
     render() {
         return (
             <div className="registerForm">
             <Form>
                 <FormGroup>
-                    <label htmlFor="#quantity">How much food are you requesting?</label>
-                    <FormInput id="#quantity"  type="number" placeholder="Quantity" /> kgs
+                    <label htmlFor="quantity">How much food are you requesting?</label>
+                    <FormInput id="quantity"  type="number" placeholder="Quantity" /> kgs
                 </FormGroup>
                 <FormGroup>
-                    <label htmlFor="#DeliveryMethod">How Will this be delivered?</label>
-                    <FormSelect>
-                        <option value="true">Delivery</option>
-                        <option value="false">Pickup</option>
-                    </FormSelect>
+                    <label htmlFor="maxDistance">Maximum Distance Range</label>
+                    <FormInput id="maxDistance" type="number" placeholder="Max Distance"/> kms
                 </FormGroup>
                 <Button size="lg">Submit</Button>
             </Form>
